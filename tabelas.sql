db8aad5fae898a087af4e14e612ab19eb2ce54fb
PedroYagi <49000417+PedroYagi@users.noreply.github.com>
2022-10-26 14:01:10 -0300

    Update tabelas.sql

--- tabelas.sql
+++ tabelas.sql
@@ -1,4 +1,4 @@
-CREATE TABLE cardapioCampus1(
+CREATE TABLE cardapio(
 	prato_proteico varchar(50) not null,
 	vegetariano varchar (50) not null,
 	vegano varchar (50) not null,
@@ -10,20 +10,41 @@ CREATE TABLE cardapioCampus1(
 	salada3 varchar (50) not null,
 	salada4 varchar (50) not null,
 	sobremesa varchar (50) not null,
-	dia date not null primary key
+	dia_cardapio date not null primary key
 );
 
-CREATE TABLE cardapioCampus2(
-	prato_proteico varchar(50) not null,
-	vegetariano varchar (50) not null,
-	vegano varchar (50) not null,
-	guarnicao varchar (50) not null,
-	arroz varchar (50) not null,
-	feijao varchar (50) not null,
-	salada1 varchar (50) not null,
-	salada2 varchar (50) not null,
-	salada3 varchar (50) not null,
-	salada4 varchar (50) not null,
-	sobremesa varchar (50) not null,
-	dia date not null primary key	
+create table usuario(
+	nome_usuario varchar(30) not null,
+	email_usuario varchar(30) not null,
+	senha_usuario varchar(30) not null,
+	saldo_usuario real not null default 0,
+	id_usuario serial not null primary key
+);
+
+create table avaliacao(
+	nota_prato_proteico int,
+	nota_vegetariano int,
+	nota_vegano int,
+	nota_arroz int,
+	nota_feijao int,
+	nota_guarnicao int,
+	nota_salada int,
+	nota_sobremesa int,
+	comentario varchar (300),
+	dia_cardapio date not null, 					--referencia o dia_cardapio na tabela cardapio
+	id_usuario int not null, 					--referencia o id_usuario na tabela usuario
+	id_avaliacao serial not null primary key
+);
+
+create table forum(
+	mensagem varchar (300),
+	id_usuario int not null, 					--referencia o id_usuario na tabela usuario
+	id_forum serial not null primary key
+);
+
+create table historico(
+	saldo real,
+	dia_historico date,
+	id_usuario int not null, 					--referencia o id_usuario na tabela usuario
+	id_historico serial not null primary key
 );
